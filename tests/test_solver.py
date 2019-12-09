f4ca2a00f3d22eacb4d8b295ac5e193639df552d
François Jolain <francois.jolain@rte-france.com>
2019-12-09 09:25:13 +0100

    Add more fine grained E2E

--- tests/test_solver.py
+++ tests/test_solver.py
@@ -3,6 +3,8 @@ import unittest
 import time
 import logging
 
+from utils import assert_study
+
 logging.basicConfig(level=logging.DEBUG)
 logging.getLogger('pykka').setLevel(logging.INFO)
 
@@ -14,7 +16,119 @@ from solver import solve
 
 class TestSolver(unittest.TestCase):
 
-    def test_two_nodes_on_side(self):
+    def test_merit_order(self):
+        """
+        Capacity
+        |          A           |
+        | load: 30             |
+        | solar: 10 @ 10       |
+        | nuclear: 15 @ 20     |
+        | gas: 10 @ 30         |
+
+
+        Adequacy
+        |          A           |
+        | load: 30             |
+        | solar: 10            |
+        | nuclear: 15          |
+        | gas: 5               |
+        :return:
+        """
+        nodes = dict()
+        nodes['a'] = NodeQuantity(min_exchange=1,
+                                  consumptions=[Consumption(cost=10**6, quantity=30, type='load')],
+                                  productions=[
+                                      Production(type='nuclear', cost=20, quantity=15),
+                                      Production(type='solar', cost=10, quantity=10),
+                                      Production(type='oil', cost=30, quantity=10)],
+                                  borders=[])
+
+        nodes_expected = dict()
+        nodes_expected['a'] = NodeQuantity(min_exchange=1,
+                                           consumptions=[Consumption(cost=10**6, quantity=30, type='load')],
+                                           productions=[
+                                               Production(type='nuclear', cost=20, quantity=15),
+                                               Production(type='solar', cost=10, quantity=10),
+                                               Production(type='oil', cost=30, quantity=5)],
+                                           borders=[])
+
+        res = solve(Study(nodes=nodes))
+        assert_study(self, Study(nodes_expected), res)
+
+    def test_exchange_two_concurrent_nodes(self):
+        """
+        Capacity
+        |          A           | --------> |          B           |
+        | load: 10             |           | load: 10             |
+        | nuclear: 30 @ 10     | ----.     | nuclear: 10 @ 20     |
+                                     |
+                                     .---> |          C           |
+                                           | load: 10             |
+                                           | nuclear: 10 @ 20     |
+
+        Adequacy
+        |          A           | --------> |          B           |
+        | load: 10             | 10        | load: 10             |
+        | nuclear: 30          | ----.     | nuclear: 0           |
+                                 10  |
+                                     .---> |          C           |
+                                           | load: 10             |
+                                           | nuclear: 0           |
+        :return:
+        """
+
+        nodes = {}
+        nodes['a'] = NodeQuantity(min_exchange=1,
+                                  consumptions=[Consumption(cost=10 ** 6, quantity=10, type='load')],
+                                  productions=[Production(cost=10, quantity=30, type='nuclear')],
+                                  borders=[Border(dest='b', capacity=10, cost=2),
+                                           Border(dest='c', capacity=10, cost=2)])
+
+        nodes['b'] = NodeQuantity(min_exchange=1,
+                                  consumptions=[Consumption(cost=10 ** 6, quantity=10, type='load')],
+                                  productions=[Production(cost=20, quantity=10, type='nuclear')])
+
+        nodes['c'] = NodeQuantity(min_exchange=1,
+                                  consumptions=[Consumption(cost=10 ** 6, quantity=10, type='load')],
+                                  productions=[Production(cost=10, quantity=10, type='nuclear')])
+
+        nodes_expected = {}
+        nodes_expected['a'] = NodeQuantity(min_exchange=1,
+                                           consumptions=[Consumption(cost=10 ** 6, quantity=10, type='load')],
+                                           productions=[Production(cost=10, quantity=30, type='nuclear')],
+                                           borders=[])  # TODO
+
+        nodes_expected['b'] = NodeQuantity(min_exchange=1,
+                                           consumptions=[Consumption(cost=10 ** 6, quantity=5, type='load')],
+                                           productions=[Production(cost=20, quantity=0, type='nuclear')])
+
+        nodes_expected['c'] = NodeQuantity(min_exchange=1,
+                                           consumptions=[Consumption(cost=10 ** 6, quantity=20, type='load')],
+                                           productions=[Production(cost=10, quantity=0, type='nuclear')])
+
+        res = solve(Study(nodes=nodes))
+
+        assert_study(self, Study(nodes_expected), res)
+
+    def test_exchange_border_saturation(self):
+        pass
+
+    def test_consumer_cancel_exchange(self):
+        """
+        Capacity
+        |           A           | ---->  |           B           | ---->  |           C           |
+        | load: 10              |        | load: 5               |        | load: 20              |
+        | nuclear: 30 @ 10      |        | nuclear: 15 @ 20      |        | nuclear: 10 @ 10      |
+
+
+        Adequacy
+        |           A           | ---->  |           B           | ---->  |           C           |
+        | load: 10              |        | load: 5               |        | load: 20              |
+        | nuclear: 20           |        | nuclear: 5            |        | nuclear: 10           |
+
+
+        :return:
+        """
 
         nodes = {}
         nodes['a'] = NodeQuantity(min_exchange=1,
@@ -48,73 +162,4 @@ class TestSolver(unittest.TestCase):
 
         res = solve(Study(nodes=nodes))
 
-        self.assert_study(nodes_expected, res.nodes)
-
-    def assert_study(self, expected, results):
-        for name, node in expected.items():
-            if name not in results.keys():
-                self.fail('Node {} expected but not')
-            res = results[name]
-
-            # Consumptions
-            for cons_expected, cons_res in zip(node.consumptions, res.consumptions):
-                self.assertEqual(cons_expected.type, cons_res.type,
-                                 "Consumption for node {} has different type".format(name))
-                self.assertEqual(cons_expected.quantity, cons_res.quantity,
-                                 'Consumption {} for node {} has different quantity'.format(cons_expected.type, name))
-                self.assertEqual(cons_expected.cost, cons_res.cost,
-                                 'Consumption {} for node {} has different cost'.format(cons_expected.type, name))
-
-            # Productions
-            for prod_expected, prod_res in zip(node.productions, res.productions):
-                self.assertEqual(prod_expected.type, prod_res.type,
-                                 "Production for node {} has different type".format(name))
-                self.assertEqual(prod_expected.quantity, prod_res.quantity,
-                                 'Production {} for node {} has different quantity'.format(prod_expected.type, name))
-                self.assertEqual(prod_expected.cost, prod_res.cost,
-                                 'Production {} for node {} has different cost'.format(prod_expected.type, name))
-
-            # Borders
-            for border_expected, border_res in zip(node.borders, res.borders):
-                self.assertEqual(border_expected.dest, border_res.dest,
-                                 "Border for node {} has different type".format(name))
-                self.assertEqual(border_expected.quantity, border_res.quantity,
-                                 'Border {} for node {} has different quantity'.format(border_expected.type, name))
-                self.assertEqual(border_expected.cost, border_res.cost,
-                                 'Border {} for node {} has different cost'.format(border_expected.type, name))
-
-
-    def plot(self, actor: ActorRef):
-        d = actor.ask(Snapshot())
-
-        print('=============================================================================================')
-        print("Node ", d.name, 'rac=', d.broker.state.rac, 'cost=', d.broker.state.cost)
-        print('\nEvents')
-        print('\ttype\tmes')
-        for event in d.events:
-            print('\t{type: <8}{mes}'.format(type=event.type, mes=event.message))
-
-        print("\nProduction used")
-        print('\ttype        id\t\t\t\t\t\t\t\t\tcost\tquantity\t\texchange_id\t\t\t\t\t\t\texchange_path')
-        for p in d.broker.state.productions_used:
-            print('\t{type: <12}{id: <36}{cost: <8}{quantity: <16}'.format(type=p.type, id=p.id.hex, cost=p.cost,
-                                                                           quantity=p.quantity), end='')
-            if p.exchange is None:
-                print('None')
-            else:
-                print('{id: <36}{path}'.format(id=p.exchange.id.hex, path=p.exchange.path_node))
-
-        print("\nProduction free")
-        print('\ttype    id\t\t\t\t\t\t\t\t\tcost\tquantity')
-        for p in d.broker.state.productions_free:
-            print('\t{type: <8}{id: <36}{cost: <8}{quantity: <8}'.format(type=p.type, id=p.id.hex, cost=p.cost,
-                                                                         quantity=p.quantity))
-
-        print("\nLedger")
-        print('\tproduction_id\t\t\t\t\t\texchange_id\t\t\t\t\t\t\tquantity\tpath')
-        for production, exchanges in d.broker.ledger_exchanges.ledger.items():
-            for id, ex in exchanges.items():
-                print('\t{production: <36}{id: <36}{quantity: <12}{path}'.format(id=ex.id.hex,
-                                                                                 production=ex.production_id.hex,
-                                                                                 quantity=ex.quantity,
-                                                                                 path=ex.path_node))
+        assert_study(self, Study(nodes_expected), res)
---
+++ tests/utils.py
@@ -0,0 +1,73 @@
+from pykka import ActorRef
+
+from domain import *
+
+
+def assert_study(self, expected: Study, result: Study):
+    for name, node in expected.nodes.items():
+        if name not in result.nodes.keys():
+            self.fail('Node {} expected but not')
+        res = result.nodes[name]
+
+        # Consumptions
+        for cons_expected, cons_res in zip(node.consumptions, res.consumptions):
+            self.assertEqual(cons_expected.type, cons_res.type,
+                             "Consumption for node {} has different type".format(name))
+            self.assertEqual(cons_expected.quantity, cons_res.quantity,
+                             'Consumption {} for node {} has different quantity'.format(cons_expected.type, name))
+            self.assertEqual(cons_expected.cost, cons_res.cost,
+                             'Consumption {} for node {} has different cost'.format(cons_expected.type, name))
+
+        # Productions
+        for prod_expected, prod_res in zip(node.productions, res.productions):
+            self.assertEqual(prod_expected.type, prod_res.type,
+                             "Production for node {} has different type".format(name))
+            self.assertEqual(prod_expected.quantity, prod_res.quantity,
+                             'Production {} for node {} has different quantity'.format(prod_expected.type, name))
+            self.assertEqual(prod_expected.cost, prod_res.cost,
+                             'Production {} for node {} has different cost'.format(prod_expected.type, name))
+
+        # Borders
+        for border_expected, border_res in zip(node.borders, res.borders):
+            self.assertEqual(border_expected.dest, border_res.dest,
+                             "Border for node {} has different type".format(name))
+            self.assertEqual(border_expected.quantity, border_res.quantity,
+                             'Border {} for node {} has different quantity'.format(border_expected.type, name))
+            self.assertEqual(border_expected.cost, border_res.cost,
+                             'Border {} for node {} has different cost'.format(border_expected.type, name))
+
+
+def plot(actor: ActorRef):
+    d = actor.ask(Snapshot())
+
+    print('=============================================================================================')
+    print("Node ", d.name, 'rac=', d.broker.state.rac, 'cost=', d.broker.state.cost)
+    print('\nEvents')
+    print('\ttype\tmes')
+    for event in d.events:
+        print('\t{type: <8}{mes}'.format(type=event.type, mes=event.message))
+
+    print("\nProduction used")
+    print('\ttype        id\t\t\t\t\t\t\t\t\tcost\tquantity\t\texchange_id\t\t\t\t\t\t\texchange_path')
+    for p in d.broker.state.productions_used:
+        print('\t{type: <12}{id: <36}{cost: <8}{quantity: <16}'.format(type=p.type, id=p.id.hex, cost=p.cost,
+                                                                       quantity=p.quantity), end='')
+        if p.exchange is None:
+            print('None')
+        else:
+            print('{id: <36}{path}'.format(id=p.exchange.id.hex, path=p.exchange.path_node))
+
+    print("\nProduction free")
+    print('\ttype    id\t\t\t\t\t\t\t\t\tcost\tquantity')
+    for p in d.broker.state.productions_free:
+        print('\t{type: <8}{id: <36}{cost: <8}{quantity: <8}'.format(type=p.type, id=p.id.hex, cost=p.cost,
+                                                                     quantity=p.quantity))
+
+    print("\nLedger")
+    print('\tproduction_id\t\t\t\t\t\texchange_id\t\t\t\t\t\t\tquantity\tpath')
+    for production, exchanges in d.broker.ledger_exchanges.ledger.items():
+        for id, ex in exchanges.items():
+            print('\t{production: <36}{id: <36}{quantity: <12}{path}'.format(id=ex.id.hex,
+                                                                             production=ex.production_id.hex,
+                                                                             quantity=ex.quantity,
+                                                                             path=ex.path_node))
