31caa9cc4a5cb08072b5293db7813b56244f51fc
François Jolain <fjolain@gmail.com>
2020-02-21 18:32:06 +0100

    WIP: add border aggregator function

--- hadar/aggregator/result.py
+++ hadar/aggregator/result.py
@@ -1,4 +1,4 @@
-from typing import Union, TypeVar, List, Generic, Tuple
+from typing import Union, TypeVar, List, Generic, Tuple, Type
 
 import pandas as pd
 import numpy as np
@@ -9,8 +9,9 @@ T = TypeVar('T')
 
 
 class Index(Generic[T]):
-    def __init__(self, index: Union[List[T], T] = None):
+    def __init__(self, column, index: Union[List[T], T] = None):
         self.all = False
+        self.column = column
         if index is None:
             self.all = True
         elif not isinstance(index, list):
@@ -18,29 +19,43 @@ class Index(Generic[T]):
         else:
             self.index = index
 
+    def filter(self, df: pd.DataFrame) -> pd.Series:
+        if self.all:
+            return df[self.column].notnull()
+        return df[self.column].isin(self.index)
+
 
 class NodeIndex(Index[str]):
     def __init__(self, index: Union[List[str], str] = None):
-        Index.__init__(self, index=index)
+        Index.__init__(self, column='node', index=index)
+
+
+class TypeIndex(Index[str]):
+    def __init__(self, index: Union[List[str], str] = None):
+        Index.__init__(self, column='type', index=index)
 
 
 class TimeIndex(Index[int]):
     def __init__(self, index: Union[List[int], int] = None, start: int = None, end: int = None):
-        if index is None:
-            if start is None and end is not None:
-                raise ValueError('Please give an start index if you give an end index')
-            if start is not None and end is None:
-                raise ValueError('Please give an end index if you give a start index')
+        if index is None and start is None and end is None:
+            Index.__init__(self, column='t')
+        elif index is None:
+            if start is None:
+                raise ValueError('Please give an start index')
+            if end is None:
+                raise ValueError('Please give an end index')
             index = list(range(start, end))
-        Index.__init__(self, index)
+        Index.__init__(self, column='t', index=index)
 
 
 class ResultAggregator:
     def __init__(self, study: Study, result: Result):
         self.result = result
         self.study = study
-        self.nodes = {}
-        self.timestamp = {}
+
+        self.consumption = self._build_consumption()
+        self.production = self._build_production()
+        self.border = self._build_border()
 
     def _build_consumption(self):
         h = self.study.horizon
@@ -63,6 +78,78 @@ class ResultAggregator:
                 n_cons += 1
 
         return cons
-    
-    def aggregate(self, indexes: List[Index]) -> Tuple[pd.DataFrame]:
-        pass
+
+    def _build_production(self):
+        h = self.study.horizon
+        s = h * sum([len(n.productions) for n in self.result.nodes.values()])
+        prod = {'cost': np.empty(s), 'avail': np.empty(s), 'used': np.empty(s),
+                'type': np.empty(s), 'node': np.empty(s), 't': np.empty(s)}
+        prod = pd.DataFrame(data=prod)
+
+        n_prod = 0
+        for n, name in enumerate(self.result.nodes.keys()):
+            for i, c in enumerate(self.result.nodes[name].productions):
+                slices = prod.index[n_prod * h: (n_prod + 1) * h]
+                prod.loc[slices, 'cost'] = c.cost
+                prod.loc[slices, 'type'] = c.type
+                prod.loc[slices, 'node'] = name
+                prod.loc[slices, 'avail'] = self.study.nodes[name].productions[i].quantity
+                prod.loc[slices, 'used'] = c.quantity
+                prod.loc[slices, 't'] = np.arange(h)
+
+                n_prod += 1
+
+        return prod
+
+    def _build_border(self):
+        h = self.study.horizon
+        s = h * sum([len(n.borders) for n in self.result.nodes.values()])
+        border = {'cost': np.empty(s), 'avail': np.empty(s), 'used': np.empty(s),
+                  'src': np.empty(s), 'dest': np.empty(s), 't': np.empty(s)}
+        border = pd.DataFrame(data=border)
+
+        n_border = 0
+        for n, name in enumerate(self.result.nodes.keys()):
+            for i, c in enumerate(self.result.nodes[name].borders):
+                slices = border.index[n_border * h: (n_border + 1) * h]
+                border.loc[slices, 'cost'] = c.cost
+                border.loc[slices, 'dest'] = c.dest
+                border.loc[slices, 'src'] = name
+                border.loc[slices, 'avail'] = self.study.nodes[name].borders[i].quantity
+                border.loc[slices, 'used'] = c.quantity
+                border.loc[slices, 't'] = np.arange(h)
+
+                n_border += 1
+
+        return border
+
+    @staticmethod
+    def _pivot(i0: Index, i1: Index, i2: Index, df: pd.DataFrame) -> pd.DataFrame:
+        return pd.pivot_table(data=df[i0.filter(df) & i1.filter(df) & i2.filter(df)],
+                              index=[i0.column, i1.column, i2.column], aggfunc=lambda x: x[0])
+
+    @staticmethod
+    def _check_index(i0: Index, i1: Index, i2: Index, type: Type):
+        if not (isinstance(i0, type) or isinstance(i1, type) or isinstance(i2, type)):
+            raise ValueError('Indexes must contain a {}'.format(type.__class__.__name__))
+
+    def agg_cons(self, i0: Index, i1: Index, i2: Index) -> pd.DataFrame:
+        ResultAggregator._check_index(i0, i1, i2, TimeIndex)
+        ResultAggregator._check_index(i0, i1, i2, NodeIndex)
+        ResultAggregator._check_index(i0, i1, i2, TypeIndex)
+
+        return ResultAggregator._pivot(i0, i1, i2, self.consumption)
+
+    def agg_prod(self, i0: Index, i1: Index, i2: Index) -> pd.DataFrame:
+        ResultAggregator._check_index(i0, i1, i2, TimeIndex)
+        ResultAggregator._check_index(i0, i1, i2, NodeIndex)
+        ResultAggregator._check_index(i0, i1, i2, TypeIndex)
+
+        return ResultAggregator._pivot(i0, i1, i2, self.production)
+
+    def agg_border(self, i0: Index, i1: Index, i2: Index) -> pd.DataFrame:
+        ResultAggregator._check_index(i0, i1, i2, TimeIndex)
+        # TODO ResultAggregator._check_index(i0, i1, i2, SrcIndex)
+        # TODO ResultAggregator._check_index(i0, i1, i2, DestIndex)
+
+        return ResultAggregator._pivot(i0, i1, i2, self.production)
--- tests/aggregator/test_result.py
+++ tests/aggregator/test_result.py
@@ -1,7 +1,7 @@
 import unittest
 import pandas as pd
 
-from hadar.aggregator.result import Index, TimeIndex, ResultAggregator
+from hadar.aggregator.result import Index, TimeIndex, ResultAggregator, NodeIndex, TypeIndex
 from hadar.solver.input import *
 from hadar.solver.output import *
 
@@ -9,17 +9,26 @@ from hadar.solver.output import *
 class TestIndex(unittest.TestCase):
 
     def test_no_parameters(self):
-        self.assertEqual(True, Index().all)
+        self.assertEqual(True, Index(column='i').all)
 
     def test_on_element(self):
-        i = Index(index='fr')
+        i = Index(column='i', index='fr')
         self.assertEqual(False, i.all)
         self.assertEqual(['fr'], i.index)
 
     def test_list(self):
-        i = Index(index=['fr', 'be'])
+        i = Index(column='i', index=['fr', 'be'])
         self.assertEqual(False, i.all)
         self.assertEqual(['fr', 'be'], i.index)
+
+    def test_filter(self):
+        i = Index(column='i', index=['fr', 'be'])
+        df = pd.DataFrame(data={'i': ['it', 'fr', 'fr', 'be', 'de', 'it', 'be'],
+                                'a': [0, 1, 2, 3, 4, 5, 6]})
+
+        exp = pd.Series(data=[False, True, True, True, False, False, True], index=[0, 1, 2, 3, 4, 5, 6], name='i')
+
+        pd.testing.assert_series_equal(exp, i.filter(df))
 
 
 class TestTimeIndex(unittest.TestCase):
@@ -34,7 +43,7 @@ class TestTimeIndex(unittest.TestCase):
         self.assertEqual([2, 3, 4, 5], i.index)
 
     def test_list(self):
-        i = Index(index=[2, 6])
+        i = TimeIndex(index=[2, 6])
         self.assertEqual(False, i.all)
         self.assertEqual([2, 6], i.index)
 
@@ -54,7 +63,7 @@ class TestAggregator(unittest.TestCase):
         out = {
             'a': OutputNode(consumptions=[OutputConsumption(cost=10 ** 6, quantity=[120, 12], type='load'),
                                           OutputConsumption(cost=10 ** 6, quantity=[130, 13], type='car')],
-                            productions=[OutputProduction(cost=10, quantity=[130,12], type='prod')],
+                            productions=[OutputProduction(cost=10, quantity=[130, 13], type='prod')],
                             borders=[OutputBorder(dest='b', quantity=[110, 11], cost=2),
                                      OutputBorder(dest='c', quantity=[120, 12], cost=2)],
                             rac=[0, 0], cost=[0, 0]),
@@ -67,7 +76,6 @@ class TestAggregator(unittest.TestCase):
         self.result = Result(nodes=out)
 
     def test_build_consumption(self):
-
         # Expected
         exp = pd.DataFrame(data={'cost': [10 ** 6] * 6,
                                  'asked': [20, 2, 30, 3, 20, 2],
@@ -81,3 +89,55 @@ class TestAggregator(unittest.TestCase):
 
         pd.testing.assert_frame_equal(exp, cons)
 
+    def test_build_production(self):
+        # Expected
+        exp = pd.DataFrame(data={'cost': [10, 10, 20, 20, 20, 20],
+                                 'avail': [30, 3, 10, 1, 20, 2],
+                                 'used': [130, 13, 110, 11, 120, 12],
+                                 'type': ['prod', 'prod', 'prod', 'prod', 'nuclear', 'nuclear'],
+                                 'node': ['a', 'a', 'b', 'b', 'b', 'b'],
+                                 't': [0, 1, 0, 1, 0, 1]}, dtype=float)
+
+        agg = ResultAggregator(study=self.study, result=self.result)
+        prod = agg._build_production()
+
+        pd.testing.assert_frame_equal(exp, prod)
+
+    def test_build_border(self):
+        # Expected
+        exp = pd.DataFrame(data={'cost': [2, 2, 2, 2],
+                                 'avail': [10, 1, 20, 2],
+                                 'used': [110, 11, 120, 12],
+                                 'src': ['a', 'a', 'a', 'a'],
+                                 'dest': ['b', 'b', 'c', 'c'],
+                                 't': [0, 1, 0, 1]}, dtype=float)
+
+        agg = ResultAggregator(study=self.study, result=self.result)
+        border = agg._build_border()
+
+        pd.testing.assert_frame_equal(exp, border)
+
+    def test_aggregate_cons(self):
+        # Expected
+        index = pd.MultiIndex.from_tuples((('a', 'load', 0.0), ('a', 'load', 1.0)), names=['node', 'type', 't'], )
+        exp_cons = pd.DataFrame(data={'asked': [20, 2],
+                                      'cost': [10 ** 6] * 2,
+                                      'given': [120, 12]}, dtype=float, index=index)
+
+        agg = ResultAggregator(study=self.study, result=self.result)
+        cons = agg.agg_cons(i0=NodeIndex(index='a'), i1=TypeIndex(index='load'), i2=TimeIndex())
+
+        pd.testing.assert_frame_equal(exp_cons, cons)
+
+    def test_aggregate_prod(self):
+        # Expected
+        index = pd.MultiIndex.from_tuples((('a', 'prod', 0.0), ('a', 'prod', 1.0), ('b', 'prod', 0.0), ('b', 'prod', 1.0)),
+                                          names=['node', 'type', 't'], )
+        exp_cons = pd.DataFrame(data={'avail': [30, 3, 10, 1],
+                                      'cost': [10, 10, 20, 20],
+                                      'used': [130, 13, 110, 11]}, dtype=float, index=index)
+
+        agg = ResultAggregator(study=self.study, result=self.result)
+        cons = agg.agg_prod(i0=NodeIndex(index=['a', 'b']), i1=TypeIndex(index='prod'), i2=TimeIndex())
+
+        pd.testing.assert_frame_equal(exp_cons, cons)
