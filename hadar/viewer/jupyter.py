56f372bbb6227fc1a5786cac4d40693d40106c4c
François Jolain <fjolain@gmail.com>
2020-02-26 18:45:38 +0100

    Begin jupyter plotting system

--- hadar/viewer/jupyter.py
+++ hadar/viewer/jupyter.py
@@ -1,7 +1,79 @@
 import plotly.graph_objects as go
+import ipywidgets as widgets
+from IPython.display import display, clear_output
 
 from hadar.aggregator.result import *
 
 
-def plot_flow(agg: ResultAggregator, node: str, t: int):
-    pass
+class Plotting:
+    def __init__(self, agg, unit_quantity: str = '', time_start=None, time_end=None):
+        self.agg = agg
+        self.unit = '(%s)' % unit_quantity if unit_quantity != '' else ''
+        time = [time_start is None, time_end is None]
+        if time == [True, False] or time == [False, True]:
+            raise ValueError('You have to give both time_start and time_end')
+        elif time == [False, False]:
+            self.time_index = pd.date_range(start=time_start, end=time_end, periods=self.agg.study.horizon)
+        else:
+            self.time_index = np.arange(self.agg.study.horizon)
+
+    def dropmenu(self, plot, items):
+        menu = widgets.Dropdown(options=items, value=items[0],
+                                description='Node:', disabled=False)
+        output = widgets.Output()
+
+        def _plot(select):
+            with output:
+                clear_output()
+                fig = plot(select)
+                fig.show()
+
+        def _on_event(event):
+            if event['name'] == 'value' and event['type'] == 'change':
+                _plot(event['new'])
+
+        menu.observe(_on_event)
+        display(menu, output)
+        _plot(items[0])
+
+    def time_stack(self, node: str = None):
+        if node is not None:
+            return self.time_stack_fig(node).show()
+        else:
+            nodes = list(self.agg.result.nodes.keys())
+            self.dropmenu(self.time_stack_fig, nodes)
+
+    def time_stack_fig(self, node: str):
+        fig = go.Figure()
+        stack = np.zeros(self.agg.study.horizon)
+
+        # stack production with area
+        prod = self.agg.agg_prod(NodeIndex(node), TypeIndex(), TimeIndex()).sort_values('cost', ascending=True)
+        for i, type in enumerate(prod.index.get_level_values('type').unique()):
+            stack += prod.loc[type]['used'].values
+            fig.add_trace(go.Scatter(x=self.time_index, y=stack, name=type, mode='markers',
+                                     fill='tozeroy' if i == 0 else 'tonexty'))
+
+        # add import on stack with area
+        balance = self.agg.get_balance(node=node)
+        im = -np.clip(balance, None, 0)
+        if not (im == 0).all():
+            stack += im
+            fig.add_trace(go.Scatter(x=self.time_index, y=stack, name='import', mode='markers', fill='tonexty'))
+
+        # Reset stack, plot consumptions
+        stack = np.zeros_like(stack)
+        cons = self.agg.agg_cons(NodeIndex(node), TypeIndex(), TimeIndex()).sort_values('cost', ascending=False)
+        for i, type in enumerate(cons.index.get_level_values('type').unique()):
+            stack += cons.loc[type]['given'].values
+            fig.add_trace(go.Scatter(x=self.time_index, y=stack, name=type, line=dict(width=4, dash='dash')))
+
+        # Add export in consumption stack
+        exp = np.clip(balance, 0, None)
+        if not (exp == 0).all():
+            stack += exp
+            fig.add_trace(go.Scatter(x=self.time_index, y=stack, name='export', line=dict(width=4, dash='dash')))
+
+        fig.update_layout(title_text='Stack error for node %s' % node,
+                          yaxis_title="Quantity %s" % self.unit, xaxis_title="time")
+        return fig
--- tests/viewer/test_jupyter.py
+++ tests/viewer/test_jupyter.py
@@ -1,29 +1,49 @@
 import unittest
+import hashlib
 import pandas as pd
 import numpy as np
+
+import plotly.graph_objects as go
+from plotly.offline.offline import plot
 
 from hadar.solver.input import *
 from hadar.solver.output import *
 from hadar.solver.study import solve
 from hadar.aggregator.result import *
-from viewer.jupyter import plot_flow
+from hadar.viewer.jupyter import Plotting
 
 
 class TestJupyter(unittest.TestCase):
     def setUp(self) -> None:
-        self.study = Study(['a', 'b', 'c']) \
+        self.study = Study(['a', 'b']) \
             .add_on_node('a', data=Consumption(cost=10 ** 6, quantity=[20, 2], type='load')) \
             .add_on_node('a', data=Consumption(cost=10 ** 6, quantity=[30, 3], type='car')) \
-            .add_on_node('a', data=Production(cost=10, quantity=[30, 3], type='prod')) \
-            .add_on_node('b', data=Consumption(cost=10 ** 6, quantity=[20, 2], type='load')) \
+            .add_on_node('a', data=Production(cost=10, quantity=[60, 3], type='prod')) \
+            .add_on_node('b', data=Consumption(cost=10 ** 6, quantity=[40, 2], type='load')) \
             .add_on_node('b', data=Production(cost=20, quantity=[10, 1], type='prod')) \
             .add_on_node('b', data=Production(cost=20, quantity=[20, 2], type='nuclear')) \
-            .add_border(src='a', dest='b', quantity=[10, 1], cost=2) \
-            .add_border(src='a', dest='c', quantity=[20, 2], cost=2)
+            .add_border(src='a', dest='b', quantity=[10, 1], cost=2)
 
         self.result = solve(study=self.study)
 
         self.agg = ResultAggregator(self.study, self.result)
+        self.plot = Plotting(agg=self.agg, unit_quantity='MW', time_start='2020-02-01', time_end='2020-02-02')
 
-    def test_flow(self):
-        plot_flow(self.agg, 'a', 0)
+        self.hash = hashlib.sha3_256()
+
+    def test_time_stack(self):
+        fig = self.plot.time_stack_fig('a')
+        self.assert_fig('be9217b194e459e3bea13fe8a1f664312f9a93f0', fig)
+
+    def assert_fig(self, expected: str, fig: go.Figure):
+        h = hashlib.sha1()
+        h.update(TestJupyter.get_html(fig).encode('ascii'))
+        self.assertEqual(expected, h.hexdigest())
+
+    @staticmethod
+    def get_html(fig: go.Figure):
+        html = plot(fig, include_plotlyjs=False, include_mathjax=False, output_type='div')
+        # plotly use a random id. We need to extract it and replace it by constant
+        # uuid can be find at ... <div id="xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx" class="plotly-graph-div" ...
+        uuid = html.split('" class="plotly-graph-div"')[0][-36:]
+        return html.replace(uuid, '#####')
