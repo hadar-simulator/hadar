d45d68d4facbf98844064226c19149b77817cef3
François Jolain <4466185+FrancoisJ@users.noreply.github.com>
2020-07-10 10:43:27 +0200

    #79 fix import in sphinx

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = 'Except where otherwise noted, this content is Copyright (c) 2020, R
 author = 'RTE'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.0'
+release = hadar.__version__
 
 
 # -- General configuration ---------------------------------------------------
